0fee4ffa10527cd192cee5917df9064923b75e18
RyuseiNomi <kanontonto1010@gmail.com>
2020-08-29 19:09:29 +0900

    register subcommands

--- main.go
+++ main.go
@@ -1,21 +1,56 @@
 package main
 
 import (
-	"github.com/RyuseiNomi/stash/src/handler"
 	"log"
 	"os"
+
+	"github.com/RyuseiNomi/stash/src/handler"
 
 	cli "github.com/urfave/cli/v2"
 )
 
 func main() {
 	app := &cli.App{
-		Name:  "stash",
-		Usage: "an effective tool to stash your work",
-		Action: func(c *cli.Context) error {
-			sw := handler.NewStashWorker(c.Args().Get(0))
-			sw.Handle()
-			return nil
+		Name:    "stash",
+		Usage:   "an effective tool to stash your work",
+		Version: "1.0.0",
+		Commands: []*cli.Command{
+			{
+				Name:  "save",
+				Usage: "Save your current work",
+				Action: func(c *cli.Context) error {
+					sw := handler.NewStashWorker("save")
+					sw.Handle()
+					return nil
+				},
+			},
+			{
+				Name:  "pop",
+				Usage: "Fetch work which is saved temporarily",
+				Action: func(c *cli.Context) error {
+					sw := handler.NewStashWorker("pop")
+					sw.Handle()
+					return nil
+				},
+			},
+			{
+				Name:  "list",
+				Usage: "Show the list of stashed work",
+				Action: func(c *cli.Context) error {
+					sw := handler.NewStashWorker("list")
+					sw.Handle()
+					return nil
+				},
+			},
+			{
+				Name:  "show",
+				Usage: "Show the detail difference each saved work",
+				Action: func(c *cli.Context) error {
+					sw := handler.NewStashWorker("show")
+					sw.Handle()
+					return nil
+				},
+			},
 		},
 	}
 
