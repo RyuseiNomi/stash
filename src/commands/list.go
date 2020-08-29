e4f474fad7d911131dcd0bd616fb4281ce378966
RyuseiNomi <kanontonto1010@gmail.com>
2020-08-29 14:50:55 +0900

    implementation of list command

--- src/commands/list.go
+++ src/commands/list.go
@@ -1,2 +1,17 @@
 package commands
 
+import (
+	"fmt"
+	"os/exec"
+)
+
+// List stashされている内容をリストとして表示する
+func List() {
+	fmt.Println("現在Stashされている内容は以下のとおりです。")
+	fmt.Println("-------------------------------------")
+	out, err := exec.Command("git", "stash", "list").Output()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(out))
+}
